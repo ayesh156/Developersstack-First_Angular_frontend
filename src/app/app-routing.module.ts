9c762a0d95517d62a7a353eab9b781315a504088
= <=>
2024-06-18 09:09:51 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'doctor', loadChildren: () => import('./modules/doctor/doctor.module').then(m => m.DoctorModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
